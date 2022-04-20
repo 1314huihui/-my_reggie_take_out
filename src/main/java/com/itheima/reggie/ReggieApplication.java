65ee7382ad4fce0b64f8d3ba2d4b3b3f81ffe00e
mygit <397608301@qq.com>
2022-04-20 18:21:40 +0800

    登录页面完成

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
+
         log.info("项目启动成功");
     }
 }
